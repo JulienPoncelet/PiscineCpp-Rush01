4b3336407eb208ea9a654e214b946074a7a1a3df
Julien-jacques PONCELET <jponcele@e2r6p1.42.fr>
2015-01-17 11:06:02 +0100

    Module Hostname

--- headers/ft_gkrellm.hpp
+++ headers/ft_gkrellm.hpp
@@ -3,15 +3,18 @@
 
 # include <iostream>
 # include <sstream>
+# include <unistd.h>
 
 enum displayType_e{
 	SIMPLE = 1, POURCENTAGE, GRAPHICAL
 };
 
 class IMonitorModule;
+class AModuleSimple;
 class ModuleHostname;
 
 # include <IMonitorModule.hpp>
+# include <AModuleSimple.hpp>
 # include <ModuleHostname.hpp>
 
 #endif
